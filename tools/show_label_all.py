50fb98bcc5502cad73d95c4cc3ab4926bed609d7
Chengyong Lin <chgyg@qq.com>
2024-04-07 00:21:07 +0800

    show_label_all: Refactor part of the code about P

--- tools/show_label_all.py
+++ tools/show_label_all.py
@@ -8,6 +8,10 @@ import sys
 import time
 import json
 
+import scipy
+from scipy.optimize import minimize
+from scipy.spatial.distance import cdist
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
@@ -17,6 +21,9 @@ from utils.general import Pcalib, visualize_court, court
 
 # python show_label_all.py ./dataset/match1/videos/1_10_12.mp4 --label dataset/match/labels
 # 若不加label,则会默认从mp4文件同级目录读取
+
+COLOR_calc = (0, 255, 0)
+COLOR_label = (0, 255, 255)
 
 
 # state 0:hidden  1:visible
@@ -40,6 +47,79 @@ keybindings = {
 
     'quit':          [ ord('q'), ],
 }
+
+def reprojection_error(P, points3D, points2D):
+    P = P.reshape(3, 4)
+    projected_points = np.dot(P, np.hstack((points3D, np.ones((points3D.shape[0], 1)))).T).T
+    projected_points = projected_points[:, :2] / projected_points[:, 2][:, np.newaxis]
+    error = np.linalg.norm(projected_points - points2D)
+    return error
+
+def ransac_reprojection(initial_guess_P, points_3d, points_2d, threshold=5, max_iterations=10):
+    best_P = None
+    best_inliers = []
+
+    for i in range(max_iterations):
+        # 随机采样4个点
+        sample_indices = np.random.choice(len(points_3d), size=6, replace=False)
+        sample_points_3d = points_3d[sample_indices]
+        sample_points_2d = points_2d[sample_indices]
+
+        # 计算投影矩阵
+        ransac_result = minimize(reprojection_error, initial_guess_P, args=(sample_points_3d, sample_points_2d), method='BFGS')
+        P = ransac_result.x.reshape(3, 4)
+
+        # 计算所有点的重投影误差
+        uv2 = np.dot( P, np.concatenate( (points_3d.T, np.ones((1, len(points_3d)))) ) )
+        uv2 = ((uv2 / uv2[2, :]).T).astype(np.int32)
+        all_errors = np.sqrt(np.sum((uv2[:, :2] - points_2d)**2, axis=1))
+
+        # 计算内点
+        inliers = np.where(all_errors < threshold)[0]
+
+        # 如果内点数量超过当前最优解，则更新最优解
+        if len(inliers) > len(best_inliers):
+            best_P = P
+            best_inliers = inliers
+
+    return best_P, best_inliers
+
+def fit_3d_2d(kps_int, num, Ps):
+    kps_court = np.zeros((32, 7), dtype=np.float32)
+    kps_court[:, :3] = kps_int
+    kps_court[:, 3:6] = court
+    kps_court[:, 6] = [i for i in range(32)]
+
+    selection = kps_court[kps_court[:, 2] == 1]
+    uv = selection[:, :2]
+    xyz = selection[:, 3:6]
+    mask_label = selection[:, 6]
+
+    if kps_int[30][2]==0 or kps_int[31][2]==0:
+        if num == 0:
+            initial_guess_P = None
+        else:
+            if Ps[-1] is not None:
+                initial_guess_P = Ps[-1].ravel()
+            else:
+                initial_guess_P = None
+
+        result = scipy.optimize.minimize(reprojection_error, initial_guess_P, args=(xyz, uv))
+        P = result.x.reshape(3, 4)
+        # best_P, inliers = ransac_reprojection(initial_guess_P, xyz, uv)
+    else:
+
+        P, err = Pcalib(xyz, uv)
+
+    uv2 = np.dot( P, np.concatenate( (xyz.T, np.ones((1, len(xyz)))) ) )
+    uv2 = ((uv2 / uv2[2, :]).T).astype(np.int32)
+    errors = np.sqrt(np.sum((uv2[:, :2] - uv)**2, axis=1))
+    errors = np.vstack((errors, mask_label)).T
+    errors = errors[errors[:, 0] > 15]
+
+    return P, errors
+
+
 
 
 class VideoPlayer():
@@ -71,9 +151,10 @@ class VideoPlayer():
         self.Ps = []
         self.kps_all_ori = []
         self.ball = []
+        self.all_errors = []
 
         if opt.label is None:
-            self.ball = pd.read_csv(opt.csv)[['Visibility', 'X', 'Y']].values.astype(int)
+            self.ball = pd.read_csv(opt.csv)[['Visibility', 'X', 'Y']].fillna(0).values.astype(np.int32)
 
             for num in range(self.frames):
                 num_json = os.path.join(opt.court, "{}.json".format(num))
@@ -94,34 +175,17 @@ class VideoPlayer():
                             assert index <= 31 and index >= 0
 
                             kps[index] = point
-                        
-                        self.kps_all_ori.append(kps)
 
-                        if kps[30][2]==0 or kps[31][2]==0:
-                            self.Ps.append(None)
-                        else:
-                            kps_int = np.array(kps).astype(int)
-                            kps_court = np.zeros((32, 6), dtype=np.float32)
-                            kps_court[:, :3] = kps_int
-                            kps_court[:, 3:6] = court
+                        kps_int = np.array(kps).astype(int)
+                        self.kps_all_ori.append(kps_int)
 
-                            selection = kps_court[kps_court[:, 2] == 1]
-                            uv = selection[:, :2]
-                            xyz = selection[:, 3:6]
-                            P, err = Pcalib(xyz, uv)
+                        P, errors = fit_3d_2d(kps_int, num, self.Ps)
 
-                            self.Ps.append(P)
-
-                            # uv2 = np.dot( P, np.concatenate( (court.T, np.ones((1, len(court)))) ) )
-                            # uv2 = np.array((uv2 / uv2[2, :]).T).astype(np.int32)
-                            # uv2 = np.clip(uv2, -9999, 9999)
-
-                            # kps_int[:, :2] = uv2[:, 0:2]
-                            # self.kps_all.append(kps_int)
+                        self.Ps.append(P)
+                        self.all_errors.append(errors)
                 else:
                     print("warning: {} is not found".format(num_json))
-                    self.Ps.append(None)
-                    self.kps_all_ori.append(None)
+                    exit()
 
         else:
             assert(os.path.exists(opt.label))
@@ -139,52 +203,25 @@ class VideoPlayer():
                     assert len(kps_frac) == 32
                     kps_int = (kps_frac * np.array([self.width, self.height, 1]).T).astype(int)
 
-                    kps_court = np.zeros((32, 6))
-                    kps_court[:, :3] = kps_int
-                    kps_court[:, 3:6] = court
+                    self.kps_all_ori.append(kps_int)
 
-                    selection = kps_court[kps_court[:, 2] == 1]
-                    uv = selection[:, :2]
-                    xyz = selection[:, 3:6]
-                    P, err = Pcalib(xyz, uv)
+                    P, errors = fit_3d_2d(kps_int, num, self.Ps)
 
-                    uv2 = np.dot( P, np.concatenate( (court.T, np.ones((1, len(court)))) ) )
-                    uv2 = np.array((uv2 / uv2[2, :]).T).astype(np.int32)
-                    uv2 = np.clip(uv2, -9999, 9999)
-
-                    kps_int[:, :2] = uv2[:, 0:2]
-                    self.kps_all.append(kps_int)
+                    self.Ps.append(P)
+                    self.all_errors.append(errors)
 
         self.display()
 
 
-    def save_piece(self):
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.piece_start)
-
-        out = cv2.VideoWriter('{}_{}.mp4'.format(self.piece_start+1, self.piece_end+1), self.fourcc, self.fps, (self.width, self.height))
-
-        frame_cnt = self.piece_start
-        while frame_cnt <= self.piece_end:
-            ret, frame = self.cap.read()
-            out.write(frame)
-
-            frame_cnt += 1
-        
-        out.release()
-
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_num)
-        print("save piece succefully!")
-
-
     def display(self):
         res_frame = self.frame.copy()
-        # res_frame = cv2.putText(res_frame, state_name[self.info['visible'][self.frame_num]], (100, 110), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2, cv2.LINE_AA)
         res_frame = cv2.putText(res_frame, "Frame: {}/{}".format(int(self.frame_num), int(self.frames-1)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.circle(res_frame, (50, 70), 3, (0, 255, 0), -1)
-        res_frame = cv2.putText(res_frame, "Current frame", (70, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.circle(res_frame, (50, 100), 3, (0, 255, 255), -1)
-        res_frame = cv2.putText(res_frame, "Previous frame", (70, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-        # res_frame = cv2.putText(res_frame, "Piece: {}-{}".format(int(self.piece_start+1), int(self.piece_end+1)), (100, 170), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.circle(res_frame, (50, 70), 3, COLOR_calc, -1)
+        res_frame = cv2.putText(res_frame, "Calc Pos", (70, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR_calc, 2, cv2.LINE_AA)
+
+        cv2.circle(res_frame, (50, 100), 3, COLOR_label, -1)
+        res_frame = cv2.putText(res_frame, "Label Pos", (70, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR_label, 2, cv2.LINE_AA)
 
         vis = self.ball[self.frame_num][0]
         x = self.ball[self.frame_num][1]
@@ -193,38 +230,33 @@ class VideoPlayer():
             cv2.circle(res_frame, (x, y), self.circle_size, (0, 0, 255), -1)
 
         P = self.Ps[self.frame_num]
-        kps_int = self.kps_all_ori[self.frame_num]
-        if kps_int is not None:
-            if P is not None:
-                kps_int = np.array(self.kps_all_ori[self.frame_num]).astype(np.int32)
+        kps_ori = np.array(self.kps_all_ori[self.frame_num]).astype(np.int32)
 
-                uv2 = np.dot( P, np.concatenate( (court.T, np.ones((1, len(court)))) ) )
-                uv2 = np.array((uv2 / uv2[2, :]).T).astype(np.int32)
-                uv2 = np.clip(uv2, -9999, 9999)
+        kps_int = np.array(self.kps_all_ori[self.frame_num]).astype(np.int32)
+        uv2 = np.dot( P, np.concatenate( (court.T, np.ones((1, len(court)))) ) )
+        uv2 = np.array((uv2 / uv2[2, :]).T).astype(np.int32)
+        uv2 = np.clip(uv2, -9999, 9999)
+        kps_int[:, :2] = uv2[:, 0:2]
 
-                kps_int[:, :2] = uv2[:, 0:2]
-                res_frame = visualize_court(res_frame, kps_int)
-            else:
-                if self.frame_num >= 1:
-                    kps_int = np.array(self.kps_all_ori[self.frame_num]).astype(np.int32)
-                    kps_int_pre = np.array(self.kps_all_ori[self.frame_num-1]).astype(np.int32)
+        res_frame = visualize_court(res_frame, kps_int)
 
-                    for i in range(32):
-                        x, y, v = kps_int[i][0], kps_int[i][1], kps_int[i][2]
-                        xo, yo, vo = kps_int_pre[i][0], kps_int_pre[i][1], kps_int_pre[i][2]
+        errors = self.all_errors[self.frame_num]
+        for err, label_index in errors:
+            x, y = kps_int[int(label_index)][0], kps_int[int(label_index)][1]
+            xo, yo = kps_ori[int(label_index)][0], kps_ori[int(label_index)][1]
 
-                        if v:
-                            cv2.circle(res_frame, (x, y), 3, (0, 255, 0), -1)
-                        if vo:
-                            cv2.circle(res_frame, (xo, yo), 3, (0, 255, 255), -1)
-                        if v and vo:
-                            cv2.line(res_frame, (x, y), (xo, yo), (0, 0, 255), 2)
+            cv2.line(res_frame, (x, y), (xo, yo), (0, 0, 255), 2)
+
+            cv2.circle(res_frame, (x, y), 3, COLOR_calc, -1)
+            cv2.circle(res_frame, (xo, yo), 3, COLOR_label, -1)
+            res_frame = cv2.putText(res_frame, "{}-{:.1f}".format(int(label_index), err), (x+5, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255))
+            print("label index:{}, label pos:({},{}), calc pos:({},{}), err pix:{:.1f}".format(int(label_index), xo, yo, x, y, err))
 
         cv2.imshow('Frame', res_frame)
 
     #    frame_num   0---->frames-1
     def main_loop(self):
-        key = cv2.waitKeyEx(1)
+        key = cv2.waitKeyEx(0)
         if key in keybindings['first_frame']:
             self.frame_num = 0
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_num)
@@ -287,18 +319,9 @@ class VideoPlayer():
             self.circle_size -= 1
 
 
-        elif key in keybindings['piece_start']:
-            self.piece_start = self.frame_num
-
-        elif key in keybindings['piece_end']:
-            self.piece_end = self.frame_num
-            self.save_piece()
-
-
         elif key in keybindings['quit']:
             self.finish()
             return
-
 
         self.display()
 
