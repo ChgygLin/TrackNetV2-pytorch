8920ea7f386b9618c21bed493caca73275728e45
ChgygLin <chgyg@qq.com>
2024-04-07 17:26:59 +0800

    small fix

--- tools/imgs2video.py
+++ tools/imgs2video.py
@@ -9,6 +9,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('imgs_base', type=str, default=None, help='Path to the imgs dir.')
     parser.add_argument('--ori', type=str, default=None, help='ori video file.')
+    parser.add_argument('--fps', type=str, default=None, help='fps')
     opt = parser.parse_args()
     return opt
 
@@ -18,17 +19,22 @@ if __name__ == '__main__':
     assert(opt.imgs_base is not None)
 
     fps = 30
-    if opt.ori is None:
-        opt.ori = opt.imgs_base.replace("images", "videos")
-        opt.ori = f"{opt.ori}.mp4"
+    if opt.fps is not None:
+        fps = int(opt.fps)
+    else:
+        if opt.ori is None:
+            opt.ori = opt.imgs_base.replace("images", "videos")
+            opt.ori = f"{opt.ori}.mp4"
 
-        if os.path.exists(opt.ori):
-            print(f"found ori video {opt.ori}")
+            if os.path.exists(opt.ori):
+                print(f"found ori video {opt.ori}")
+            else:
+                opt.ori = None
 
-    if opt.ori is not None:
-        cap = cv2.VideoCapture(opt.ori)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        print(f"get ori video {opt.ori} fps: {fps}")
+        if opt.ori is not None:
+            cap = cv2.VideoCapture(opt.ori)
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            print(f"get ori video {opt.ori} fps: {fps}")
 
     _, file_name = os.path.split(opt.imgs_base)
 
